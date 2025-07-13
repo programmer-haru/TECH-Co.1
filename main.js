e5dc64a0dc2a8dc2dece5e852086d5a8ca717269
programmer-haru <nanyaodou9@gmail.com>
2025-07-14 02:19:32 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,26 @@
   toggle.addEventListener('click', () => {
     header.classList.toggle('open');
   });
+
+  
+  { 
+  function callback(entries,obs){
+    entries.forEach(entry => {
+      if(!entry.isIntersecting){
+        return;
+      }
+      entry.target.classList.add("appear");
+      obs.unobserve(entry.target);
+    });
+  }
+
+
+  const observer = new IntersectionObserver(callback, {
+    threshold:1,
+  });
+
+
+  document.querySelectorAll(".fadein").forEach(el => {
+  observer.observe(el);
+  });
+}
