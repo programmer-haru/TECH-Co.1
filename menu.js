24dee35fd8f0b2351d88b704ff078f9624a7f15d
programmer-haru <nanyaodou9@gmail.com>
2025-12-15 17:31:34 +0900

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
+
+// メニュークリックで閉じる
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+  });
+});
